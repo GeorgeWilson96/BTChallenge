926321b811770d0d88ace1f81058959ade52d964
Muhammed Patel <Muhammed Patel>
2016-11-17 15:07:21 +0000

    added and linkd icons

--- www/react/Screen.jsx
+++ www/react/Screen.jsx
@@ -5,6 +5,10 @@ var Router = require('./components/Router').Router;
 var router = require('./components/Router').router;
 var IndexPage = require('./pages/IndexPage');
 var SearchPage = require('./pages/SearchPage');
+var DateTime = require('./pages/DateTimePage');
+var FindPerson = require('./pages/FindPerson');
+var ProfilePage = require('./pages/ProfilePage');
+var HelpPage = require('./pages/HelpPage');
 var Screen = React.createClass({
   render:function(){
     return (<div>
@@ -12,6 +16,10 @@ var Screen = React.createClass({
       <Router router={router}>
         <IndexPage path="/" router={router}/>
         <SearchPage path="/search" router={router}/>
+        <DateTime path="/datetime" router={router}/>
+        <FindPerson path="/findperson" router={router}/>
+        <ProfilePage path="/profile" router={router}/>
+        <HelpPage path="/help" router={router}/>
       </Router>
     </div>);
   }
