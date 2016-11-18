3ca3fa397018fb6ab3a5849242ebc76b38f00eb3
Muhammed Patel <Muhammed Patel>
2016-11-18 00:37:23 +0000

    added date and #buggycheckboxes

--- www/react/pages/DateTimePage.jsx
+++ www/react/pages/DateTimePage.jsx
@@ -2,8 +2,15 @@ var React  = require('react');
 var TopBar = require('../components/TopBar');
 var DateTimePage = React.createClass({
   render:function(){
-    return (<div>
-
+    return (<div id="Date">
+        <input type="date" name="inDate"></input>
+        <input type="date" name="outDate"></input>
+        <div id= "checkboxes">
+        <input type="checkbox" value="Morning (07:00 - 12:00)"></input>
+        <input type="checkbox" value="Afternoon (12:00 - 17:00)"></input>
+        <input type="checkbox" value="Evening (17:00 - 22:00)"></input>
+        <input type="checkbox" value="Overnight (22:00 - 07:00"></input></div>
+        {/*<a onClick={()=>{this.props.router.goto("/FloorPlanPage");}}><div id = "fplanbutton"><button className = "ui button fluid homeButton">Next</button></div></a>*/}
     </div>);
   }
 });
